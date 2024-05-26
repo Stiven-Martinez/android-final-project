27eece2ad151447b9a603fc0456a8e0b7f25d25a
Luis Vega <HonorVex03@gmail.com>
2024-05-26 00:15:11 -0700

    First commit

---
+++ app/src/main/java/org/example/evaluacion/Onboarding1Fragment.java
@@ -0,0 +1,46 @@
+package org.example.evaluacion;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Onboarding1Fragment extends Fragment {
+    Button botonSiguiente;
+    Button botonSkip;
+    NavController navController;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment);
+            }
+        });
+        botonSkip = view.findViewById(R.id.botonSkip);
+        botonSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding1Fragment_to_login);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/org/example/evaluacion/Onboarding3Fragment.java
@@ -0,0 +1,46 @@
+package org.example.evaluacion;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Onboarding3Fragment extends Fragment {
+    Button botonSiguiente;
+    Button botonSkip;
+    NavController navController;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding3Fragment_to_login);
+            }
+        });
+        botonSkip = view.findViewById(R.id.botonSkip);
+        botonSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding3Fragment_to_login);
+            }
+        });
+    }
+}
