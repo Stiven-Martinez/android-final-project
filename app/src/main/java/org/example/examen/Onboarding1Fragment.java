e5e171e5e75c9bff939d7f13f54f9f37b93971ee
Stiven <128733721+Stiven-Martinez@users.noreply.github.com>
2024-05-27 16:42:51 -0700

    Registro de nuevo usuario

--- app/src/main/java/org/example/examen/Onboarding1Fragment.java
+++ app/src/main/java/org/example/examen/Onboarding1Fragment.java
@@ -21,23 +21,14 @@ public class Onboarding1Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle
             savedInstanceState) {
         return inflater.inflate(R.layout.fragment_onboarding1, container, false);
-    } @
-            Override
+    }
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
         botonSiguiente = view.findViewById(R.id.botonSiguiente);
-        botonSiguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment);
-            }});
+        botonSiguiente.setOnClickListener(view1 -> navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment));
         botonSkip = view.findViewById(R.id.botonSkip);
-        botonSkip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.navigate(R.id.action_onboarding1Fragment_to_login);
-            }
-        });
+        botonSkip.setOnClickListener(view2 -> navController.navigate(R.id.action_onboarding1Fragment_to_login));
     }
 }
---
+++ app/src/main/java/org/example/examen/Registro.java
@@ -0,0 +1,55 @@
+package org.example.examen;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Registro extends Fragment {
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mBtnLogin;
+    private NavController navController;
+    private String sharedPrefFile = "com.example.examen";
+    private SharedPreferences mPreferences;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registro, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        mUsername = view.findViewById(R.id.username_input);
+        mPassword = view.findViewById(R.id.password_input);
+        mBtnLogin = view.findViewById(R.id.login_btn);
+
+        mBtnLogin.setOnClickListener(v -> {
+            mPreferences = requireContext().getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE);
+
+            SharedPreferences.Editor editor = mPreferences.edit();
+            editor.putString("username", mUsername.getText().toString());
+            editor.putString("password", mPassword.getText().toString());
+            editor.putBoolean("newAccount", true);
+            editor.apply();
+
+            navController.navigate(R.id.action_registro_to_login);
+        });
+    }
+}
